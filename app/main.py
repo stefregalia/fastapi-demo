2268d44e1bf43a935b31c4b24bb91e7077d5f01e
stefregalia <xtm9px@virginia.edu>
2024-11-19 12:29:33 -0500

    FastAPI complete

--- app/main.py
+++ app/main.py
@@ -1,21 +1,93 @@
 #!/usr/bin/env python3
 
-from fastapi import FastAPI
-from typing import Optional
-from pydantic import BaseModel
-import json
 import os
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import mysql.connector
+from mysql.connector import Error
+from fastapi.middleware.cors import CORSMiddleware
+
+
+load_dotenv()
+
+
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "admin"
+DBPASS = os.getenv("DBPASS")
+DB = "xtm9px"
+
 
 app = FastAPI()
 
-@app.get("/")  # zone apex
-def zone_apex():
-    return {"Good Day": "Sunshine!"}
 
-@app.get("/sum/{a}/{b}")
-def add(a: int, b: int):
-    return {"sum": a + b}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
-@app.get("/multiply/{c}/{d}")
-def multiply(c: int, d: int):
-    return {"product": c * d}
+
+def get_db_connection():
+    try:
+        db = mysql.connector.connect(
+            user=DBUSER,
+            host=DBHOST,
+            password=DBPASS,
+            database=DB
+        )
+        cursor = db.cursor()
+        return db, cursor
+    except Error as e:
+        print(f"Error: {e}")
+        return None, None
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the DIY Spotify API!"}
+
+
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    db, cursor = get_db_connection()
+    if not db or not cursor:
+        return {"Error": "Failed to connect to the database"}
+    
+    try:
+        cursor.execute(query)
+        headers = [x[0] for x in cursor.description]
+        results = cursor.fetchall()
+        json_data = [dict(zip(headers, result)) for result in results]
+        return json_data
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+    finally:
+        cursor.close()
+        db.close()
+
+
+@app.get('/songs')
+def get_songs():
+    query = """
+        SELECT songs.title, songs.album, songs.artist, songs.year,
+               songs.file, songs.image, genres.genre
+        FROM songs
+        JOIN genres ON songs.genre = genres.genreid
+    """
+    db, cursor = get_db_connection()
+    if not db or not cursor:
+        return {"Error": "Failed to connect to the database"}
+    
+    try:
+        cursor.execute(query)
+        headers = [x[0] for x in cursor.description]
+        results = cursor.fetchall()
+        json_data = [dict(zip(headers, result)) for result in results]
+        return json_data
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+    finally:
+        cursor.close()
+        db.close()
